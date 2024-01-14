17e5566a39f0cdcc35acafe33c36e0e4a07e8809
Aaron Li <a1li@ucsd.edu>
2024-01-14 13:19:49 -0800

    TST style and addons, some settings, etc.

--- firefox.nix
+++ firefox.nix
@@ -18,6 +18,9 @@
           multi-account-containers
           tree-style-tab
 
+          # TST add-ons
+          tst-wheel-and-double
+
           # Theme
           theme-nord-polar-night
 
@@ -30,6 +33,7 @@
           privacy-badger
 
           # QOL
+          auto-tab-discard
           darkreader
           dearrow
           sponsorblock
@@ -154,20 +158,81 @@
           "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # Enable userChrome and userContent
           "layout.css.has-selector.enabled" = true; # For conditionally hiding tabs
 
+          # Customization
+          "browser.theme.content-theme" = 0; # Why doesn't this apply?
+          "browser.theme.toolbar-theme" = 0;
+          "browser.toolbars.bookmarks.visibility" = "newtab";
+          # No annoying sponsored/Pocket stuff
           "extensions.pocket.enabled" = false;
-
           "browser.newtabpage.activity-stream.showSponsored" = false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
 
+          # Tracking protection
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.emailtracking.enabled" = true;
           "privacy.trackingprotection.socialtracking.enabled" = true;
         };
 
         userChrome = ''
-          /* Hide horizontal tabs at top of the window when TST visible */
+          /*
+          Hide horizontal tabs at top of the window when TST visible
+          https://github.com/piroor/treestyletab/wiki/Code-snippets-for-custom-style-rules#hide-horizontal-tabs-at-the-top-of-the-window-1349-1672-2147
+          */
           html#main-window body:has(#sidebar-box[sidebarcommand=treestyletab_piro_sakura_ne_jp-sidebar-action][checked=true]) #TabsToolbar {
             visibility: collapse !important;
+          }
+
+          /*
+          Auto show/hide theme
+          Requires additional CSS in TST addon preferences
+          https://github.com/piroor/treestyletab/wiki/Code-snippets-for-custom-style-rules#full-auto-showhide-theme
+          */
+
+          /* Hide main tabs toolbar */
+          #main-window[tabsintitlebar="true"]:not([extradragspace="true"]) #TabsToolbar > .toolbar-items {
+            opacity: 0;
+            pointer-events: none;
+          }
+          #main-window:not([tabsintitlebar="true"]) #TabsToolbar {
+            visibility: collapse !important;
+          }
+
+          /* Sidebar min and max width removal */
+          #sidebar-box {
+            max-width: none !important;
+            min-width: 0px !important;
+          }
+          /* Hide splitter, when using Tree Style Tab. */
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] + #sidebar-splitter {
+            display: none !important;
+          }
+          /* Hide sidebar header, when using Tree Style Tab. */
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+            visibility: collapse;
+          }
+
+          /* Shrink sidebar until hovered, when using Tree Style Tab. */
+          :root {
+            --thin-tab-width: 4rem;
+            --wide-tab-width: 25rem;
+          }
+          #sidebar-box:not([sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"]) {
+            min-width: var(--wide-tab-width) !important;
+            max-width: none !important;
+          }
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] {
+            position: relative !important;
+            transition: all 100ms !important;
+            min-width: var(--thin-tab-width) !important;
+            max-width: var(--thin-tab-width) !important;
+          }
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"]:hover {
+            transition: all 200ms !important;
+            transition-delay: 0.2s !important;
+            min-width: var(--wide-tab-width) !important;
+            max-width: var(--wide-tab-width) !important;
+            margin-right: calc((var(--wide-tab-width) - var(--thin-tab-width)) * -1) !important;
+            z-index: 1;
           }
         '';
         # userContent = "";
