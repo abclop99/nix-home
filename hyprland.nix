f32d64efc5e66240d1d6279b335eb438668b6935
Aaron Li <a1li@ucsd.edu>
2024-03-04 11:25:51 -0500

    Add keybinds for media keys

--- hyprland.nix
+++ hyprland.nix
@@ -155,6 +155,16 @@
 					) 10)
 				);
 
+				# Media key binds
+				bindel = [
+					", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+					", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+				];
+				bindl = [
+					", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+					", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+				];
+
 				# Move/resize mouse with mod + LMB/RMB and dragging
 				bindm = [
 					"$mod, mouse:272, movewindow"
