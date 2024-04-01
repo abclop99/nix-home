21219169e3441f96174ecc7628820bdea2e338cf
Aaron Li <a1li@ucsd.edu>
2024-03-31 20:46:07 -0400

    Change kitty font to FiraCode

--- home.nix
+++ home.nix
@@ -159,7 +159,7 @@
       };
 
       font = {
-        name = "VictorMono NF";
+        name = "FiraCode Nerd Font";
         package = pkgs.nerdfonts.override {
           fonts = [
             "FiraCode"
