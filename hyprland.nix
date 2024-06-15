97abce3ba3547589914156d69c8ca719fb90c673
Aaron Li <a1li@ucsd.edu>
2024-06-15 16:05:57 -0400

    Switch from swaylock and swayidle to hyprlock and hypridle

--- hyprland.nix
+++ hyprland.nix
@@ -72,7 +72,7 @@
 				# "$browser" = "firefox";
 				"$terminal" = "kitty";
 				"$menu" = "bemenu-run";
-				"$lock" = "swaylock";
+				"$lock" = "${pkgs.systemd}/bin/loginctl lock-session";
 				"$notif" = "swaync";
 
 				# Execute programs at launch
@@ -269,28 +269,92 @@
 		};
 
 		# Lock screen program
-		programs.swaylock = {
+		programs.hyprlock = {
 			enable = true;
-			package = pkgs.swaylock-effects;
-			settings = {
-				# Behavior
-				ignore-empty-password = true;
-				show-failed-attempts = true;
-				grace=1;
-				
-				# Appearance
-				indicator-idle-visible = true;
-				clock = true;
-				fade-in = 1;
-				color = "331100";
 
-				# TODO: Swaylock effects order mattes, nix doesn't allow multiple or different orders
-				screenshots = true;
-				# effect-scale = 0.5;
-				effect-vignette = "0.22:0.85";
-				effect-custom = "${config.xdg.configHome}/swaylock/effect-blue-filter/filter.c";
-				effect-blur = "7x5";
-				# effect-scale = 2.0;
+			# https://wiki.hyprland.org/Hypr-Ecosystem/hyprlock/
+			settings = {
+				general = {
+					grace = 2;
+					ignore_empty_input = true;
+				};
+
+				background = [
+					{
+						monitor = "";
+						path = "screenshot";
+
+						blur_passes = 1;
+						blur_size = 7;
+						noise = 0.0117;
+					}
+				];
+
+				shape = [
+					{
+						monitor = "";
+				    size = "360, 60";
+				    color = "rgba(17, 17, 17, 1.0)";
+				    rounding = -1;
+				    border_size = 8;
+				    border_color = "rgba(0, 207, 230, 1.0)";
+				    rotate = 0;
+				    xray = false; # if true, make a "hole" in the background (rectangle of specified size, no rotation)
+
+				    position = "0, 80";
+				    halign = "center";
+				    valign = "center";
+					}
+				];
+
+				label = [
+					{
+						    monitor = "";
+						    text = "Hi there, $USER";
+						    text_align = "center"; # center/right or any value for default left. multi-line text alignment inside label container
+						    color = "rgba(200, 200, 200, 1.0)";
+						    font_size = 25;
+						    font_family = "Noto Sans";
+						    rotate = 0; # degrees, counter-clockwise
+
+						    position = "0, 80";
+						    halign = "center";
+						    valign = "center";
+					}
+				];
+
+				input-field = [
+					{
+						monitor = "";
+				    size = "200, 50";
+				    outline_thickness = 3;
+				    dots_size = 0.33; # Scale of input-field height, 0.2 - 0.8
+				    dots_spacing = 0.15; # Scale of dots' absolute size, 0.0 - 1.0
+				    dots_center = false;
+				    dots_rounding = -1; # -1 default circle, -2 follow input-field rounding
+				    outer_color = "rgb(151515)";
+				    inner_color = "rgb(200, 200, 200)";
+				    font_color = "rgb(10, 10, 10)";
+				    fade_on_empty = true;
+				    fade_timeout = 1000; # Milliseconds before fade_on_empty is triggered.
+				    placeholder_text = "<i>Input Password...</i>"; # Text rendered in the input box when it's empty.
+				    hide_input = false;
+				    rounding = -1; # -1 means complete rounding (circle/oval)
+				    check_color = "rgb(204, 136, 34)";
+				    fail_color = "rgb(204, 34, 34)"; # if authentication failed, changes outer_color and fail message color
+				    fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>"; # can be set to empty
+				    fail_transition = 300; # transition time in ms between normal outer_color and fail_color
+				    capslock_color = -1;
+				    numlock_color = -1;
+				    bothlock_color = -1; # when both locks are active. -1 means don't change outer color (same for above)
+				    invert_numlock = false; # change color if numlock is off
+				    swap_font_color = false; # see below
+
+				    position = "0, -20";
+				    halign = "center";
+				    valign = "center";
+					}
+				];
 			};
 		};
 		## Needs pam access in system config for unlocking
@@ -301,29 +365,46 @@
 			./files/swaylock/effect-blue-filter;
 
 		# Swayidle
-		services.swayidle =
-		let 
-			lockProcess = "swaylock";
-			lockCommand = "${pkgs.swaylock-effects}/bin/swaylock -f";
-			dpmsCommand = "${pkgs.hyprland}/bin/hyprctl dispatch dpms";
+		services.hypridle =
+		let
+			dpms_command = "${pkgs.hyprland}/bin/hyprctl dispatch dpms";
 		in
 		{
 			enable = true;
-			systemdTarget = "hyprland-session.target"; # Uses hyprctl
-			timeouts = [
-				# Main locking timeout
-				{ timeout = 600; command = lockCommand; }
-				# Timeout after locked: turn off screen
-				{
-					timeout = 15;
-					command = "if ${pkgs.procps}/bin/pgrep -x ${lockProcess}; then ${dpmsCommand} off; fi";
-					resumeCommand = "${dpmsCommand} on";
-				}
-			];
-			events = [
-				{ event = "before-sleep"; command =  lockCommand; }
-				{ event = "lock"; command =  lockCommand; }
-			];
+
+			# https://wiki.hyprland.org/Hypr-Ecosystem/hypridle/
+			settings = {
+				general = {
+					lock_cmd = "${pkgs.procps}/bin/pgrep -x hyprlock || ${pkgs.hyprlock}/bin/hyprlock"; # Avoid starting multiple hyprlock instances
+					before_sleep_cmd = "loginctl lock-session"; # Lock before suspend
+					after_sleep_cmd = "${dpms_command} on"; # To avoid having to press a key twice to turn on the display
+				};
+
+				# Dim the screen after 2.5 minutes
+				listener = [
+					{
+						timeout = 150;       # 2.5 min
+						on-timeout = "{pkgs.brightnessctl}/bin/brightnessctl -s set 10"; # Set monitor backlight to log
+						on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";        # Restore monitor backlight
+					}
+
+					{
+						timeout = 300;       # 5 min
+						on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";  # Lock screen after timeout
+					}
+
+					{
+						timeout = 10;       # Seconds after lock/last interaction
+						on-timeout = "${pkgs.procps}/bin/pgrep -x hyprlock && ${dpms_command} off";  # Turn off screen if locked
+						on-resume = "${dpms_command} on";  # Screen on
+					}
+
+					{
+						timeout = 1800;      # 30 min
+						on-timeout = "${pkgs.systemd}/bin/systemctl suspend"; # Suspend PC
+					}
+				];
+			};
 		};
 	};
 }
